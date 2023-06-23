8297e3563dd08ecd7d7b7a95bce05bf052ca52e7
IanoNjuguna <inc3099@gmail.com>
2023-06-23 17:18:15 +0300

    index.js: get element by ID

---
+++ DOM-Manipulation/index.js
@@ -0,0 +1,14 @@
+// DOM Manipulation
+
+// getElementById()
+
+const title = document.getElementById("main-heading");
+console.log(title);
+
+// getElementsByClassName()
+
+// getElementByTagName()
+
+// querySelector()
+
+// querySelectorAll()

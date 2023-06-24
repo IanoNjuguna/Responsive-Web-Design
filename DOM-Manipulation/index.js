87c0a2f84f9cc733e0280004d6d52c59003f0b70
IanoNjuguna <inc3099@gmail.com>
2023-06-24 18:01:21 +0300

    index.js: label test

--- DOM-Manipulation/index.js
+++ DOM-Manipulation/index.js
@@ -1,26 +1,21 @@
 // DOM Manipulation
 
 // getElementById()
-
 const title = document.getElementsById("main-heading");
-console.log(title);
+console.log(title); // test
 
 // getElementsByClassName()
-
 const classItem = document.getElementsByClassName("list-item");
-console.log(classItem);
+console.log(classItem); // test
 
 // getElementByTagName()
-
 const tagItem = document.getElementsByClassName("list-item");
-console.log(tagItem);
+console.log(tagItem); //test
 
 // querySelector()
-
 const container = document.querySelector("div");
-console.log(container);
+console.log(container); //test
 
 // querySelectorAll()
-
 const container2 = document.querySelectorAll("div");
-console.log(container2);
+console.log(container2); //test

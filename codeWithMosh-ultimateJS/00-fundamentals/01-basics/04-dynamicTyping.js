4c160b4edfa780ec725be92aef2ea51462436ebb
IanoNjuguna <ichege438@gmail.com>
2024-03-01 23:32:21 +0300

    04-dynamicTyping.js: dynamic typing in JS

---
+++ codeWithMosh-ultimateJS/00-fundamentals/01-basics/04-dynamicTyping.js
@@ -0,0 +1,12 @@
+/*
+ * JS is a dynamically typed language
+ *
+ * The type of a variable can change at runtime.
+ */
+
+// The let keyword does not allow you to declare a variable in the same block. The var keyword does.
+let jina = "Iano";
+
+let type = typeof jina;
+
+console.log(type)
